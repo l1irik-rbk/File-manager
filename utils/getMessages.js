fff9181df996106e828277b4983f505d8e828526
l1irik-rbk <kir.baranow2010@gmail.com>
2022-06-10 19:10:34 +0300

    fix: user name

--- utils/getMessages.js
+++ utils/getMessages.js
@@ -1,5 +1,11 @@
-const args = process.env;
-const user = args['username'];
+const argv = process.argv;
+let user;
+
+argv.forEach((arg) => {
+  if (arg.startsWith('--username')) {
+    user = arg.split('=')[1];
+  }
+});
 
 export const getWelcomMessage = () => {
   console.log(`Welcome to the File Manager, ${user}!\n`);
